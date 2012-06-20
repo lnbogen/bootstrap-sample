b3ca98b2db4aac1553db68616989342b8eb662db
orenellenbogen <oren.ellenbogen@gmail.com>
2012-06-20 09:40:20 +0300

    use less module for less files, still not working ("\n" problem)

--- modules/press-1.0.29/app/press/ScriptFileManager.java
+++
@@ -1,8 +0,0 @@
-package press;
-
-public class ScriptFileManager extends SourceFileManager {
-    public ScriptFileManager() {
-        super("JavaScript", ScriptCompressor.EXTENSION, "#{press.script}",
-                "#{press.compressed-script}", "<!-- press-js: ", " -->", PluginConfig.js.srcDir);
-    }
-}
--- modules/press-1.0.29/app/press/StyleFileManager.java
+++
@@ -1,9 +0,0 @@
-package press;
-
-public class StyleFileManager extends SourceFileManager {
-    public StyleFileManager() {
-        super("CSS", StyleCompressor.EXTENSION, "#{press.stylesheet}",
-                "#{press.compressed-stylesheet}", "<!-- press-css: ", " -->",
-                PluginConfig.css.srcDir);
-    }
-}
